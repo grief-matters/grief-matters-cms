af0c74fc3c07d47b3ce73d6d17b8ba5128c51969
Dan Chambers <danlechambre@icloud.com>
2023-07-15 00:19:12 +0100

    Testing simple custom action

--- sanity.config.ts
+++ sanity.config.ts
@@ -1,18 +1,23 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import { visionTool } from "@sanity/vision";
+import { schemaTypes } from "./schemas";
+import { HelloWorldAction } from "./actions";
 
 export default defineConfig({
-  name: 'default',
-  title: 'grief-matters-cms',
+  name: "default",
+  title: "grief-matters-cms",
 
-  projectId: 'vg3sb730',
-  dataset: 'production',
+  projectId: "vg3sb730",
+  dataset: "production",
 
   plugins: [deskTool(), visionTool()],
 
   schema: {
     types: schemaTypes,
   },
-})
+
+  document: {
+    actions: [HelloWorldAction],
+  },
+});
