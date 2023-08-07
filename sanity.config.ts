34a75607b3817fe726245a8f3e4e4860ab70f9cb
Dan Chambers <danlechambre@icloud.com>
2023-08-07 22:27:36 +0100

    Implement singleton type for 'Organization'

--- sanity.config.ts
+++ sanity.config.ts
@@ -5,6 +5,14 @@ import { schemaTypes } from "./schemas";
 import { ConvertAction } from "./actions";
 import { INTERNET_RESOURCE_TYPES } from "./constants";
 import { InternetResourceType } from "./types";
+import { startCase } from "lodash";
+import pluralize from "pluralize";
+
+const singletonActions = new Set(["publish", "discardChanges", "restore"]);
+
+const singletonTypes = new Set(["organization"]);
+
+const nonSingletonTypes = Array.from(new Set(INTERNET_RESOURCE_TYPES));
 
 export default defineConfig({
   name: "default",
@@ -12,17 +20,44 @@ export default defineConfig({
   projectId: "vg3sb730",
   // @ts-ignore
   dataset: process.env.SANITY_STUDIO_DATASET,
-  plugins: [deskTool(), visionTool()],
+  plugins: [
+    deskTool({
+      structure: (S) =>
+        S.list()
+          .title("Content")
+          .items([
+            S.listItem()
+              .title("Organization")
+              .id("organization")
+              .child(
+                S.document()
+                  .schemaType("organization")
+                  .documentId("organization")
+              ),
+
+            ...nonSingletonTypes.map((ns) =>
+              S.documentTypeListItem(ns).title(startCase(pluralize(ns)))
+            ),
+          ]),
+    }),
+    visionTool(),
+  ],
   schema: {
     types: schemaTypes,
+    templates: (templates) =>
+      templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
   },
   document: {
     actions: (prev, context) => {
+      const nonSingletonActions = singletonTypes.has(context.schemaType)
+        ? prev.filter(({ action }) => action && singletonActions.has(action))
+        : prev;
+
       return INTERNET_RESOURCE_TYPES.includes(
         context.schemaType as InternetResourceType
       )
-        ? [...prev, ConvertAction]
-        : prev;
+        ? [...nonSingletonActions, ConvertAction]
+        : nonSingletonActions;
     },
   },
 });
