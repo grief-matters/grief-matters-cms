5090794b8973774c5d653008267a58d1d1811bee
Dan Chambers <danlechambre@icloud.com>
2023-08-17 23:02:03 +0100

    Add new content types

---
+++ schemas/booklet.ts
@@ -0,0 +1,29 @@
+import { defineField, defineType } from "sanity";
+import { internetResourcePreviewConfig } from "../configs/internetResourcePreviewConfig";
+
+export default defineType({
+  name: "booklet",
+  type: "document",
+  title: "Booklet",
+  preview: internetResourcePreviewConfig,
+  initialValue: {
+    validated: "false",
+  },
+  fields: [
+    defineField({
+      title: "Title",
+      name: "title",
+      type: "string",
+    }),
+    defineField({
+      title: "Description",
+      name: "description",
+      type: "text",
+    }),
+    defineField({
+      title: "Resource Details",
+      name: "resourceDetails",
+      type: "resourceBase",
+    }),
+  ],
+});
