d369a94426e6cf2a6d0c69fee11fc237f6e9f880
Dan Chambers <danlechambre@icloud.com>
2023-09-10 12:36:20 +0100

    Add a bunch of new fields for reusability across schemas

--- schemas/fields/index.ts
+++ schemas/fields/index.ts
@@ -1,1 +1,7 @@
 export { default as readyForReviewField } from "./readyForReviewField";
+export { default as populationsField } from "./populationsField";
+export { default as urlField } from "./urlField";
+export { default as categoriesField } from "./categoriesField";
+export { default as websiteReferenceField } from "./websiteReferenceField";
+
+export { default as duplicateReferenceValidator } from "./duplicateReferenceValidator";
