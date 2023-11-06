621342dcb23f4503eceb346adf1594e67181b9b6
LeePNolan <lpnwebdev@gmail.com>
2023-11-06 23:42:56 +0000

    #45 - regex url validation for paths feature added

--- schemas/fields/urlField.ts
+++ schemas/fields/urlField.ts
@@ -4,9 +4,18 @@ import ResourceUrlInput from "../../components/ResourceUrlInput";
 const urlField = defineField({
   title: "URL",
   description:
-    "This should be a valid web address, usually starting with 'https://'",
+    "This should be a valid web address, usually starting with 'https://'. Paths may be used but be aware this likely points to a specific resource, rather than a website overall.",
   name: "resourceUrl",
   type: "url",
+  validation: (Rule) =>
+    Rule.custom((resourceUrl) => {
+      const pattern = new RegExp("http[s]*://[^/]+(/.+)");
+      if (pattern.test(resourceUrl)) {
+        return "Warning: URL Contains A Path";
+      } else {
+        return true;
+      }
+    }),
   components: {
     input: ResourceUrlInput,
   },
