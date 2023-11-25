eb6e748321421b2f4e4dcdf91e36fd7605bea563
LeePNolan <lpnwebdev@gmail.com>
2023-11-25 22:40:25 +0000

    #95 implemented suggested changes

--- schemas/fields/urlField.ts
+++ schemas/fields/urlField.ts
@@ -4,18 +4,9 @@ import ResourceUrlInput from "../../components/ResourceUrlInput";
 const urlField = defineField({
   title: "URL",
   description:
-    "This should be a valid web address, usually starting with 'https://'. Paths may be used but be aware this likely points to a specific resource, rather than a website overall.",
+    "This should be a valid web address, usually starting with 'https://'",
   name: "resourceUrl",
   type: "url",
-  validation: (Rule) =>
-    Rule.custom((resourceUrl) => {
-      const pattern = new RegExp("http[s]*://[^/]+(/.+)");
-      if (pattern.test(resourceUrl)) {
-        return "Warning: URL Contains A Path. Consider that this may point to a resource rather than a website.";
-      } else {
-        return true;
-      }
-    }).warning(),
   components: {
     input: ResourceUrlInput,
   },
