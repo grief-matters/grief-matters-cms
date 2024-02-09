1d5df05cdfc70fff0065e8530449e7df4cd00628
Dan Chambers <danlechambre@icloud.com>
2024-02-09 23:05:05 +0000

    Add featuredContent content preview configs

---
+++ configs/resourceLinksPreviewConfig.ts
@@ -0,0 +1,23 @@
+import type { PreviewConfig } from "sanity";
+
+export const resourceLinksPreviewConfig: PreviewConfig = {
+  select: {
+    resource0: "resources.0.title",
+    resource1: "resources.1.title",
+    resource2: "resources.2.title",
+    resource3: "resources.3.title",
+  },
+  prepare: ({ resource0, resource1, resource2, resource3 }) => {
+    const resourceTitles = [resource0, resource1, resource2, resource3].filter(
+      Boolean
+    );
+    const subtitle =
+      resourceTitles.length > 0 ? `${resourceTitles.join(", ")}` : "";
+    const hasMoreLinks = Boolean(resource3);
+
+    return {
+      title: "Resource Links Collection",
+      subtitle: hasMoreLinks ? `${subtitle}…` : subtitle,
+    };
+  },
+};
--- schemas/objectSchemas/resourcePageLinks.ts
+++ schemas/objectSchemas/resourcePageLinks.ts
@@ -3,10 +3,16 @@ import { defineArrayMember, defineField, defineType } from "sanity";
 export default defineType({
   type: "object",
   name: "resourcePageLinks",
+  preview: {
+    prepare: () => ({
+      title: "Resource Page Links",
+      subtitle: "A collection of links to pages of resources",
+    }),
+  },
   fields: [
     defineField({
       name: "links",
-      title: "Resource Page Links",
+      title: "Resource Page Links Collection",
       description:
         "By selecting various options, create links to entire pages of resources",
       type: "array",
