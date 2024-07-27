1712a9a1b09f35a8d064309b6a95cba2ec2a2914
Dan Chambers <danlechambre@icloud.com>
2024-07-27 10:29:06 +0100

    Add a path based footer link to featured content panels

--- schemas/documentSchemas/featuredContent.ts
+++ schemas/documentSchemas/featuredContent.ts
@@ -33,6 +33,46 @@ export default defineType({
       title: "Footer Link",
       description: "The link to be shown in the footer of the feature panel",
       type: "resourcePageLink",
+      readOnly: (ctx) =>
+        typeof ctx.document?.manualFeaturedContentFooterLink !== "undefined",
+    }),
+    defineField({
+      name: "manualFeaturedContentFooterLink",
+      title: "Footer Link (manually set)",
+      description: "A footer link with a manually set path",
+      type: "object",
+      readOnly: (ctx) =>
+        typeof ctx.document?.featuredContentFooterLink !== "undefined",
+      fields: [
+        defineField({
+          name: "label",
+          title: "Label",
+          description: "The text that a user will see",
+          type: "string",
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: "url",
+          title: "Relative Path",
+          description: "A relative path for the desired page",
+          type: "url",
+          validation: (Rule) => Rule.uri({ relativeOnly: true }),
+        }),
+      ],
     }),
   ],
+  validation: (Rule) =>
+    Rule.custom((fields) => {
+      if (typeof fields === "undefined") {
+        return true;
+      }
+
+      const tooManyFooterFields =
+        typeof fields?.featuredContentFooterLink !== "undefined" &&
+        typeof fields?.manualFeaturedContentFooterLink !== "undefined";
+
+      return tooManyFooterFields
+        ? "You can only have a single footer field  "
+        : true;
+    }),
 });
