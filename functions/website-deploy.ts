ecf2c27c0c7175ff7dc88e5aa7035f71421ff1e4
Dan Chambers <danlechambre@icloud.com>
2025-02-04 21:27:20 +0000

    Add a web deploy pages function

---
+++ functions/website-deploy.ts
@@ -0,0 +1,24 @@
+// todo - I don't think we should have todo this as we're generating worker-configuration.d.ts automatically
+interface Env {
+  WEBSITE_DEPLOY_HOOK: string;
+}
+
+export const onRequest: PagesFunction<Env> = async (context) => {
+  const deployHookUrl = context.env.WEBSITE_DEPLOY_HOOK;
+
+  if (typeof deployHookUrl === "undefined") {
+    return new Response("Deploy hook URL is not configured", { status: 500 });
+  }
+
+  try {
+    const response = await fetch(deployHookUrl, { method: "POST" });
+
+    if (!response.ok) {
+      return new Response(`Deploy hook failed: ${response.statusText}`, {
+        status: 500,
+      });
+    }
+  } catch (error) {
+    return new Response(`Error triggering deploy: ${error}`, { status: 500 });
+  }
+};
