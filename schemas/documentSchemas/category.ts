5284ccf8e2dd3b8411e403c78c5a4f91b425a8bb
Tina Flower <tmflower@gmail.com>
2025-11-20 09:15:43 -1000

    Combine Featured Resource On Topic Schema #169

--- schemas/documentSchemas/category.ts
+++ schemas/documentSchemas/category.ts
@@ -1,6 +1,7 @@
 import { defineArrayMember, defineField, defineType } from "sanity";
 import { TagsIcon } from "@sanity/icons";
 import { featuredResourcesField, slugField, titleField } from "../fields";
+import { INTERNET_RESOURCE_TYPES } from "../../constants";
 
 export default defineType({
   name: "category",
@@ -69,7 +70,82 @@ export default defineType({
       type: "accessibleImage",
       description: "A cover image to associate with this category",
     }),
-    featuredResourcesField("article"),
-    featuredResourcesField("story"),
+    defineField({
+      name: "featuredResources",
+      title: "Featured Resources",
+      type: "array",
+      description: "Select a maximum of 3 Featured Resources in this category.",
+      of: INTERNET_RESOURCE_TYPES.map((resourceType) =>
+        defineArrayMember({
+          name: resourceType,
+          type: "reference",
+          to: [{ type: resourceType }],
+          options: {
+            filter: (resolverCtx) => {
+              const { document } = resolverCtx;
+              if (!document._id) {
+                return { filter: undefined, params: undefined };
+              }
+              const idParam = document._id.replace("drafts.", "");
+              return {
+                filter: "$id in categories[]._ref",
+                params: { id: idParam },
+              };
+            },
+          },
+        })
+      ),
+      validation: (Rule) => [
+        Rule.max(3),
+        Rule.unique(),
+        Rule.custom(async (resources, context) => {
+          if (typeof resources === "undefined" || resources.length < 1) {
+            return true;
+          }
+
+          const queryParts = resources.map((r: any) => `_id == "${r._ref}"`);
+          const query = `*[${queryParts.join(" || ")}]`;
+          console.log(query);
+
+          const client = context.getClient({
+            apiVersion: process.env.SANITY_STUDIO_API_VERSION!,
+          });
+
+          try {
+            const docs = await client.fetch(query);
+
+            if (Array.isArray(docs)) {
+              const resourcesWithoutImages = docs
+                .filter((doc) => typeof doc?.image?.asset === "undefined")
+                .map((doc) => doc.title.substring(0, 15) + "...");
+
+              return resourcesWithoutImages.length > 0
+                ? `"${resourcesWithoutImages.join(
+                    `", "`
+                  )}" do not have Images - fallbacks will be used`
+                : true;
+            }
+
+            return true;
+          } catch (error) {
+            return "Error validating this field";
+          }
+        }).warning(),
+      ],
+    }),
+    defineField({
+      ...featuredResourcesField("article"),
+      deprecated: {
+        reason:
+          "Deprecated as part of site redesign. Use Featured Resources instead.",
+      },
+    }),
+    defineField({
+      ...featuredResourcesField("story"),
+      deprecated: {
+        reason:
+          "Deprecated as part of site redesign. Use Featured Resources instead.",
+      },
+    }),
   ],
 });
