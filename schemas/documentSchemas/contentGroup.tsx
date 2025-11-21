f615ba7056de1b5c1ad0ae165983d1a5599a2c05
Dan Chambers <danlechambre@icloud.com>
2025-11-21 21:20:35 +0000

    Restore description field for content group[s for use in headers

--- schemas/documentSchemas/contentGroup.tsx
+++ schemas/documentSchemas/contentGroup.tsx
@@ -1,5 +1,6 @@
 import { defineArrayMember, defineField, defineType } from "sanity";
-import { portableTextDescriptionField, slugField } from "../fields";
+
+import { simpleDescriptionField, slugField } from "../fields";
 import { ContentGroupPreview } from "../../components";
 
 export default defineType({
@@ -35,6 +36,12 @@ export default defineType({
         "An optional title to display to users. Leave blank if you want an untitled content group.",
     }),
     defineField({
+      ...simpleDescriptionField,
+      description:
+        "Some introductory text for this Content Group. Usually shown in Page Headers (may not be shown in all circumstances)",
+      rows: 3,
+    }),
+    defineField({
       ...slugField,
       description: `Slugs are used to link specific content to a specific page e.g. a slug of 'donate' will be linked to the 'donate' page. Where there is no existing page, one will be generated. Speak to a project developer if you are unsure.`,
       validation: undefined,
@@ -54,13 +61,6 @@ export default defineType({
       of: [
         defineArrayMember({ type: "reference", to: { type: "contentBlock" } }),
       ],
-    }),
-    defineField({
-      ...portableTextDescriptionField,
-      deprecated: {
-        reason:
-          "Deprecated as part of site redesign. Use appropriate item within a Content Block instead.",
-      },
     }),
     defineField({
       deprecated: {
