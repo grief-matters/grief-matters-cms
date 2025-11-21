4fece8fffc1b39e7e90410ef9a84a2100bcc4476
Dan Chambers <danlechambre@icloud.com>
2025-11-21 21:52:16 +0000

    More tweaks to content block previews

--- schemas/objectSchemas/headingText.ts
+++ schemas/objectSchemas/headingText.ts
@@ -4,6 +4,15 @@ export default defineType({
   type: "object",
   name: "headingText",
   description: "Text to be used as a heading",
+  preview: {
+    select: {
+      text: "text",
+    },
+    prepare: ({ text }) => ({
+      title: "Heading Block",
+      subtitle: `Heading Text: ${text}`,
+    }),
+  },
   fields: [
     defineField({
       name: "text",
