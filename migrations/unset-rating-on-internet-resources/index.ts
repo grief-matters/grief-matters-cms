b7fdd7f8d57598c15077788d6efb47727278304d
Dan Chambers <danlechambre@icloud.com>
2026-05-15 12:48:24 +0100

    more migrations

---
+++ migrations/unset-rating-on-internet-resources/index.ts
@@ -0,0 +1,13 @@
+import { at, defineMigration, unset } from "sanity/migrate";
+import { INTERNET_RESOURCE_TYPES } from "../../constants";
+
+export default defineMigration({
+  title: "Unset deprecated rating field on all internet resources",
+  documentTypes: [...INTERNET_RESOURCE_TYPES],
+  filter: "defined(rating)",
+  migrate: {
+    document() {
+      return at("rating", unset());
+    },
+  },
+});
