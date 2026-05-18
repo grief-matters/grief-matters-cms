84d8b14e0986e65df77f3126d33ca919e5dbcc47
Dan Chambers <danlechambre@icloud.com>
2026-05-18 11:36:56 +0100

    archive migrations for taxonomy changes ease of managing

--- migrations/archived/unset-rating-on-internet-resources/index.ts
+++ migrations/archived/unset-rating-on-internet-resources/index.ts
@@ -1,5 +1,5 @@
 import { at, defineMigration, unset } from "sanity/migrate";
-import { INTERNET_RESOURCE_TYPES } from "../../constants";
+import { INTERNET_RESOURCE_TYPES } from "../../../constants";
 
 export default defineMigration({
   title: "Unset deprecated rating field on all internet resources",
